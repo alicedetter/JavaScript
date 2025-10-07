ce3a4b17053108ac900bb27a89fda699789a411d
alicedetter <detteralice@gmail.com>
2025-10-07 13:19:42 +0200

    h

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded",function()
 {
-    const ruta1=document.getElementbyId("ruta1");
-    const ruta2=document.getElementbyId("ruta2");
+    const ruta1=document.getElementById("ruta1");
+    const ruta2=document.getElementById("ruta2");
     ruta1.addEventListener('click',function()
     {
-        ruta2.classList.add("första");
+        ruta2.classList.add("first");
         ruta2.classList.remove("andra");
-        ruta1.classList.remove("första");
+        ruta1.classList.remove("first");
         ruta1.classList.add("andra");
-
     });
 });
